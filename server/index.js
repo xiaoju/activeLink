cca0a9aad04900b406e9a174157e95dab7dcd1da
xiaoju <me@xiaoju.io>
2017-12-19 22:38:28 +0100

    feat: build google oauth2 scaffolding

--- server/index.js
+++ server/index.js
@@ -1,10 +1,32 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello World!' })
-})
+const app = express();
 
-const PORT = process.env.PORT || 5000
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('accessToken', accessToken);
+      console.log('refresh Token', refreshToken);
+      console.log('profile', profile);
+    }
+  )
+);
 
-app.listen(PORT)
+app.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
