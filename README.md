d70474586244a0748cf69586b082a8ec02aee8e9
xiaoju <me@xiaoju.io>
2017-12-23 00:35:18 +0100

    feat: cookie based authentication

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # activeLink
+
+## app purpose
+
+* This app is a platform to manage extra-curricular activities at a school:
+  * users:
+    * plaftormMaster: manages the platform.
+    * providers: offer classes for kids, receive paiements.
+    * parents: register their kids to classes, pay for the classes.
+    * kids: attend the classes.
+
+## app architecture
+
+* front end: React, Redux
+* back end: node.js, Express, Passport, oauth2, mongoose
+* database: mongoDB (hosted on mlab.com)/ PostgreSQL
+* https
+
+## license
+
+## contributing
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,19 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +23,17 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('accessToken:', accessToken);
-      console.log('refresh Token:', refreshToken);
-      console.log('profile:', profile);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // already got record of this user
+          done(null, existingUser);
+        } else {
+          // don't have record yet
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
