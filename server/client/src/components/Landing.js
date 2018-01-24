4b218002cefa7a0c0632763f1b16f0c477105c18
xiaoju <me@xiaoju.io>
2018-01-24 15:23:03 +0100

    feat: build landing page

---
+++ server/client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>activeLink!</h1>
+      Here some text.
+    </div>
+  );
+};
+
+export default Landing;
