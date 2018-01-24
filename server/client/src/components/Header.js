928c5f73bee56a9d3a13803398e50fc1f6e19b21
xiaoju <me@xiaoju.io>
2018-01-24 15:37:35 +0100

    feat: link on logo of header page

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   renderContent() {
@@ -25,7 +26,12 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">activeLink</a>
+          <Link
+            to={this.props.auth ? '/dashboard' : '/'}
+            className="left brand-logo"
+          >
+            activeLink
+          </Link>
           <ul className="right">
             <li>{this.renderContent()}</li>
           </ul>
