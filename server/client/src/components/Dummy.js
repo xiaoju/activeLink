ec3551d29b6de7198d198fcc37e4c2a84eade382
xiaoju <me@xiaoju.io>
2018-01-27 00:46:34 +0100

    fix: dummy component

---
+++ server/client/src/components/Dummy.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dummy = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Dummy page</h1>
+    </div>
+  );
+};
+
+export default Dummy;
+
