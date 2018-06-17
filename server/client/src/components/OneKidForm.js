72eafcde9835f87fbe695291f4011838dc227b10
xiaoju <me@xiaoju.io>
2018-06-17 15:10:11 +0200

    feat: first draft of profile form

---
+++ server/client/src/components/OneKidForm.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function OneKidForm(props) {
+  return (
+    <form className="container formInputsContainer">
+      <div className="input-field">
+        <i className="material-icons prefix">face</i>
+        <input id="icon_prefix" type="text" className="validate" />
+        <label htmlFor="icon_prefix" className="active">
+          First Name
+        </label>
+      </div>
+
+      <div className="input-field">
+        {/* <i className="material-icons prefix">account_circle</i> */}
+        <input id="icon_prefix" type="text" className="validate" />
+        <label htmlFor="icon_prefix" className="active">
+          Family Name
+        </label>
+      </div>
+
+      {/* <form onSubmit={this.handleSubmit}> */}
+      <div>
+        <label>
+          {/* Select School Grade */}
+          <select
+            className="browser-default"
+
+            // value={this.state.value}
+            // onChange={this.handleChange}
+          >
+            <option value="" disabled selected>
+              School Grade 2018-1019
+            </option>
+            <option value="PS">PS</option>
+            <option value="MS">MS</option>
+            <option value="GS">GS</option>
+            <option value="CP">CP</option>
+            <option value="CE1">CE1</option>
+            <option value="CE2">CE2</option>
+            <option value="CM1">CM1</option>
+            <option value="CM2">CM2</option>
+          </select>
+        </label>
+      </div>
+      {/* <input type="submit" value="Submit" /> */}
+      {/* </form> */}
+    </form>
+  );
+}
+
+export default OneKidForm;
+
+// OneKidForm.propTypes = {
+//   itemId: PropTypes.string.isRequired,
+// };
--- server/client/src/components/ProfileForm.js
+++ server/client/src/components/ProfileForm.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import OneKidForm from './OneKidForm';
+import OneParentForm from './OneParentForm';
 // import PropTypes from 'prop-types';
 
 class ProfileForms extends Component {
@@ -6,32 +8,22 @@ class ProfileForms extends Component {
     return (
       <div className="itemsContainer">
         <h4 className="stepTitle">① Update your profile</h4>
-        <h5>
-          <strong>Parents</strong>
-        </h5>
-        <span />
-        <ul>
-          <li>mother (select mother/father/custom)</li>
-          <li>First name</li>
-          <li>Family name</li>
-          <li>Adress</li>
-          <li>Email</li>
-          <li>
-            Mobile phone (select mobile, landline, pro mobile, pro landline,
-            perso mobile, person landline, custom)
-          </li>
-        </ul>
 
         <h5>
           <strong>Kids</strong>
         </h5>
-        <ul>
-          <li>First name</li>
-          <li>Family name</li>
-          <li>
-            School grade (2018-2019) (select PS, MS, GS, CP, CE1, CE2, CM1, CM2)
-          </li>
-        </ul>
+
+        <OneKidForm />
+        <OneKidForm />
+        <OneKidForm />
+
+        <h5>
+          <strong>Parents</strong>
+        </h5>
+        <span />
+
+        <OneParentForm />
+        <OneParentForm />
 
         {/* <button
           className="btn waves-effect waves-light orange lighten-1 z-depth-2"
