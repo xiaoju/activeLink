ffb25d48f23e3275680da96b75ccaf9da50bdda9
xiaoju <me@xiaoju.io>
2018-06-17 20:43:09 +0200

    feat: conditional styling of disabled checkbox
    before this commit, there was no checkbox if kid is wrong grade. Now the
    label appears in grey, and the box as "disabled"

--- server/client/src/components/ItemCheckboxes.js
+++ server/client/src/components/ItemCheckboxes.js
@@ -20,43 +20,60 @@ function ItemCheckboxes({
       {(familyItems.includes(itemId)
         ? // handle separately items that are priced per family vs priced per kid
           [checkboxUsers[0]] // [familyId]
-        : checkboxUsers
-            .slice(1) // [kid1Id, kid2Id]
-            .filter(
-              // filter out the kids whose grade doesn't match the requested grades for this class
-              thisUserId =>
+        : checkboxUsers.slice(1)
+      ) // [kid1Id, kid2Id] // either [familyId], either [kid1Id, kid2Id], depending on the value of itemId
+        // .filter(
+        //   // filter out the kids whose grade doesn't match the requested grades for this class
+        //   thisUserId =>
+        //     items[itemId].itemGrades.includes(
+        //       familyMembers[thisUserId].kidGrade
+        //     )
+        // )
+        .map(thisUserId => (
+          <div
+            className={
+              !familyItems.includes(itemId) &&
+              items[itemId].itemGrades.includes(
+                familyMembers[thisUserId].kidGrade
+              )
+                ? 'usernameCheckboxDisabled'
+                : 'usernameCheckbox'
+            } // color of label (name of kid) is greyed out when kid not the right grade for the class
+            key={itemId + '_' + thisUserId}
+          >
+            <input
+              type="checkbox"
+              onChange={thisEvent =>
+                !mandatoryItems.includes(itemId) &&
+                (checked[thisUserId].includes(itemId)
+                  ? // if already in array
+                    uncheckCheckbox(thisUserId, itemId, thisEvent)
+                  : // if not yet in array
+                    checkCheckbox(thisUserId, itemId, thisEvent))
+              }
+              id={itemId + '_' + thisUserId}
+              className="filled-in checkbox-orange z-depth-2"
+              // TODO "z-depth-2" for shadow effect is not working!
+              // using the checkbox data from checkedReducer
+              checked={checked[thisUserId].includes(itemId) && 'checked'}
+              disabled={
+                !familyItems.includes(itemId) &&
                 items[itemId].itemGrades.includes(
                   familyMembers[thisUserId].kidGrade
-                )
-            )
-      ).map(thisUserId => (
-        <div className="usernameCheckbox" key={itemId + '_' + thisUserId}>
-          <input
-            type="checkbox"
-            onChange={thisEvent =>
-              !mandatoryItems.includes(itemId) &&
-              (checked[thisUserId].includes(itemId)
-                ? // if already in array
-                  uncheckCheckbox(thisUserId, itemId, thisEvent)
-                : // if not yet in array
-                  checkCheckbox(thisUserId, itemId, thisEvent))
-            }
-            id={itemId + '_' + thisUserId}
-            className="filled-in checkbox-orange z-depth-2"
-            // TODO "z-depth-2" for shadow effect is not working!
-            // using the checkbox data from checkedReducer
-            checked={checked[thisUserId].includes(itemId) && 'checked'}
-          />
+                ) &&
+                'disabled'
+              }
+            />
 
-          {familyItems.includes(itemId) ? (
-            <label htmlFor={itemId + '_' + thisUserId}>{familyName}</label>
-          ) : (
-            <label htmlFor={itemId + '_' + thisUserId}>
-              {familyMembers[thisUserId].firstName}
-            </label>
-          )}
-        </div>
-      ))}
+            {familyItems.includes(itemId) ? (
+              <label htmlFor={itemId + '_' + thisUserId}>{familyName}</label>
+            ) : (
+              <label htmlFor={itemId + '_' + thisUserId}>
+                {familyMembers[thisUserId].firstName}
+              </label>
+            )}
+          </div>
+        ))}
     </div>
   );
 }
