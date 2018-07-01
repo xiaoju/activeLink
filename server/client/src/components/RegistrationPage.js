5bcc4c6f48d1da70b46278880799dff035b6bb40
xiaoju <me@xiaoju.io>
2018-07-01 23:59:26 +0200

    feat: add custom favicon

--- server/client/src/components/RegistrationPage.js
+++ server/client/src/components/RegistrationPage.js
@@ -22,16 +22,16 @@ class RegistrationPage extends Component {
               <br />
               <h5 className="stepTitle">Loading...</h5>
               <br />
-              <div class="preloader-wrapper active">
-                <div class="spinner-layer spinner-red-only">
-                  <div class="circle-clipper left">
-                    <div class="circle" />
+              <div className="preloader-wrapper active">
+                <div className="spinner-layer spinner-red-only">
+                  <div className="circle-clipper left">
+                    <div className="circle" />
                   </div>
-                  <div class="gap-patch">
-                    <div class="circle" />
+                  <div className="gap-patch">
+                    <div className="circle" />
                   </div>
-                  <div class="circle-clipper right">
-                    <div class="circle" />
+                  <div className="circle-clipper right">
+                    <div className="circle" />
                   </div>
                 </div>
               </div>
