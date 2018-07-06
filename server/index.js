9877409dea73b440c3e89ca4803db1d98ea56841
xiaoju <me@xiaoju.io>
2018-07-06 17:18:50 +0200

    feat: upgrade server dependencies, incl. Mongoose

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ require('./models/User');
 require('./services/passport');
 
 // mongoose.connect(keys.mongoURI);
-mongoose.connect(keys.mongoURI, { useMongoClient: true });
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 const app = express();
 
