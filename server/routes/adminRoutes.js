859524fb489aa580a8003a519b95e8f67e70540b
xiaoju <me@xiaoju.io>
2018-08-30 12:13:36 +0200

    feat: create api/v1/dbdump endpoint

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,16 +1,54 @@
 const uuid = require('uuid4');
 const mongoose = require('mongoose');
 const Family = mongoose.model('families');
+const Asso = mongoose.model('assos');
+const User = mongoose.model('users');
 const requireLogin = require('../middlewares/requireLogin');
 const requireAdmin = require('../middlewares/requireAdmin');
 
 // TODO send mailgun emails to the newly invited families
 
 module.exports = app => {
+  // exports a full dump of the database in json format
+  app.get('/api/v1/dbdump', requireLogin, requireAdmin, async function(
+    req,
+    res
+  ) {
+    let assos, families, users, dbDump;
+
+    try {
+      assos = await Asso.find({});
+    } catch (err) {
+      console.log('dbDump error (assos): ', err);
+      assos = { failed: true, error: err.toString() };
+    }
+
+    try {
+      families = await Family.find({});
+    } catch (err) {
+      console.log('dbDump error (families): ', err);
+      families = { failed: true, error: err.toString() };
+    }
+
+    try {
+      users = await User.find({});
+    } catch (err) {
+      console.log('dbDump error (users): ', err);
+      users = { failed: true, error: err.toString() };
+    }
+
+    dbDump = { assos, families, users };
+
+    res.status(201).json({ dbDump });
+  });
+
   app.put('/api/createFamilies', requireLogin, requireAdmin, async function(
     req,
     res
   ) {
+    // takes an email addresses array and create 1 'family' database document
+    // for each email.
+
     let removeDoublons = arr => [...new Set(arr)];
 
     // ['doublon@example.com', 'doublon@example.com', 'old@example.com', 'new@example.com']
