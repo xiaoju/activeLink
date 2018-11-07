c28c4be73be29c16ee654647d1ca77f66d617b0b
xiaoju <me@xiaoju.io>
2018-11-07 23:01:14 +0100

    refactor rebuildRegistrations.js with wrapAsync

--- server/routes/apiRoutes/rebuildRegistrations.js
+++ server/routes/apiRoutes/rebuildRegistrations.js
@@ -4,48 +4,35 @@ const Asso = mongoose.model('assos');
 const Registration = mongoose.model('registrations');
 const requireLogin = require('../../middlewares/requireLogin');
 const requireAdmin = require('../../middlewares/requireAdmin');
+const wrapAsync = require('../../utils/wrapAsync');
 
-router.get('/', requireLogin, requireAdmin, async function(req, res) {
-  // take all the registrations from the asso document,
-  // and feed the Registration with those records
-  let assoId = 'a0';
-  console.log('Asso.find');
-  try {
-    thisAsso = await Asso.find({ id: assoId });
-  } catch (err) {
-    console.log('adminRoutes, 21, query error: ', err);
-    res.status(500).json({ error: err.toString() });
-  }
+router.get(
+  '/',
+  requireLogin,
+  requireAdmin,
+  wrapAsync(async function(req, res) {
+    // find all the registrations from the asso document,
+    const assoId = 'a0';
+    const thisAsso = await Asso.find({ id: assoId });
 
-  console.log('build FlatArray');
-  a0Registrations = thisAsso[0].registrations;
-  let FlatArray = Object.keys(a0Registrations)
-    .map(clientId =>
-      a0Registrations[clientId].map(itemId => ({ itemId, clientId }))
-    )
-    .reduce((output, smallArray) => output.concat(smallArray), []);
+    // reformat
+    a0Registrations = thisAsso[0].registrations;
+    let FlatArray = Object.keys(a0Registrations)
+      .map(clientId =>
+        a0Registrations[clientId].map(itemId => ({ itemId, clientId }))
+      )
+      .reduce((output, smallArray) => output.concat(smallArray), []);
 
-  let outputRemove;
-  try {
-    outputRemove = await Registration.remove({});
-  } catch (err) {
-    console.log('ERROR by mongoose remove({}): ', err);
-    res.status(500).json({ error: err.toString() });
-  }
-  console.log('deleted the existing Registration collection');
+    // delete the existing Registration collection
+    const outputRemove = await Registration.remove({});
 
-  let output;
-  try {
-    output = await Registration.insertMany(FlatArray, { ordered: false });
-  } catch (err) {
-    console.log('ERROR by mongoose insertMany(): ', err);
-    res.status(500).json({ error: err.toString() });
-  }
-  console.log('finished insertMany');
+    // insert back the whole Registration collection
+    const output = await Registration.insertMany(FlatArray, { ordered: false });
 
-  res.status(201).json({
-    FlatArray
-  });
-});
+    res.status(201).json({
+      FlatArray
+    });
+  })
+);
 
 module.exports = router;
