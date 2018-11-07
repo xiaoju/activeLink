6f600facec6871e5761cfaba4fb7a3d6abf37545
xiaoju <me@xiaoju.io>
2018-11-07 09:37:39 +0100

    refactor: dashboard.js route using wrapAsync()

--- server/routes/apiRoutes/dashboard.js
+++ server/routes/apiRoutes/dashboard.js
@@ -4,111 +4,77 @@ const Family = mongoose.model('families');
 const Asso = mongoose.model('assos');
 const User = mongoose.model('users');
 const Registration = mongoose.model('registrations');
+const wrapAsync = require('../../utils/wrapAsync');
 const requireLogin = require('../../middlewares/requireLogin');
 const requireAdmin = require('../../middlewares/requireAdmin');
 
-router.get('/', requireLogin, requireAdmin, async function(req, res) {
-  // console.log('DASHBOARD opened by ', req.user.primaryEmail);
-  let assoId = 'a0';
+router.get(
+  '/',
+  requireLogin,
+  requireAdmin,
+  wrapAsync(async function(req, res) {
+    // TODO get allKidGrades, registrationItems, classItems and volunteeringItems from Asso document
+    let allKidGrades = [
+      ['PS', 'MS', 'GS'],
+      ['CP', 'CE1', 'CE2', 'CM1', 'CM2'],
+      ['6e', '5e', '4e', '3e']
+    ];
+    let registrationItems = ['i0'];
+    let classItems = ['i1', 'i2', 'i3', 'i4', 'i5', 'i6', 'i7', 'i8'];
+    let volunteeringItems = [
+      'i9',
+      'i10',
+      'i11',
+      'i12',
+      'i13',
+      'i14',
+      'i15',
+      'i16',
+      'i17',
+      'i18',
+      'i19',
+      'i20',
+      'i21'
+    ];
 
-  let volunteers,
-    FamiliesNotRegistered,
-    FamiliesRegistered,
-    registrations,
-    csvexport,
-    itemDetails,
-    kidsByFamily,
-    parentsByFamily,
-    KidsNoPhoto,
-    usersDetails,
-    familyDetails,
-    NoPhotoconsentKids,
-    kidsQuantity,
-    kidsInClasses,
-    parentsQuantity,
-    clientsByItem;
+    // let assoId = 'a0';
+    // get the registrations, the old way (from Asso.registrations)
+    // const thisAsso = await Asso.find({ id: assoId });
+    // a0Registrations = thisAsso[0].registrations;
+    // flatRegistrations = Object.keys(a0Registrations)
+    //   .map(clientId =>
+    //     a0Registrations[clientId].map(itemId => ({ itemId, clientId }))
+    //   )
+    //   .reduce((output, smallArray) => output.concat(smallArray), []);
 
-  // get the registrations, the old way (from Asso.registrations)
-  // try {
-  //   thisAsso = await Asso.find({ id: assoId });
-  // } catch (err) {
-  //   console.log('adminRoutes, 21, query error: ', err);
-  //   res.status(500).json({ error: err.toString() });
-  // }
-  // a0Registrations = thisAsso[0].registrations;
-  // flatRegistrations = Object.keys(a0Registrations)
-  //   .map(clientId =>
-  //     a0Registrations[clientId].map(itemId => ({ itemId, clientId }))
-  //   )
-  //   .reduce((output, smallArray) => output.concat(smallArray), []);
+    // get the registrations, the new way (from Registrations collection)
+    //  const TheseRegistrations = await Registration.aggregate([
+    //     {
+    //       $group: {
+    //         _id: '$itemId',
+    //         count: { $sum: 1 }
+    //       }
+    //     },
+    //     { $sort: { _id: 1 } }
+    //     // { $project: { itemId: '$_id' } }
+    //   ]);
 
-  // get the registrations, the new way (from Registrations collection)
-  // let TheseRegistrations;
-  // try {
-  //   TheseRegistrations = await Registration.aggregate([
-  //     {
-  //       $group: {
-  //         _id: '$itemId',
-  //         count: { $sum: 1 }
-  //       }
-  //     },
-  //     { $sort: { _id: 1 } }
-  //     // { $project: { itemId: '$_id' } }
-  //   ]);
-  // } catch (err) {
-  //   console.log('adminRoutes, 102, error: ', err);
-  //   res.status(500).json({ error: err.toString() });
-  // }
-
-  // ITEMS DETAILS
-  // should save items as mongoDB collection instead redux style keyed object!
-  try {
-    itemDetails = await Asso.aggregate([
+    // TODO should save items as mongoDB collection instead redux style keyed object!
+    const itemDetails = await Asso.aggregate([
       {
         $replaceRoot: { newRoot: '$itemsById' }
       }
-      // {
-      //   $project: {
-      //     _id: 0,
-      //     id: 1,
-      //     name: 1,
-      //     period: 1,
-      //     standardPrice: 1,
-      //     discountedPrice: 1,
-      //     type: 1,
-      //     discountQualifier: 1,
-      //     mandatoryItem: 1,
-      //     audience: 1,
-      //     description: 1,
-      //     itemGrades: 1
-      //   }
-      // }
     ]);
-  } catch (err) {
-    console.log('adminRoutes, 100, error: ', err);
-    res.status(500).json({ error: err.toString() });
-  }
 
-  try {
-    usersDetails = await User.find({}).select(
+    const usersDetails = await User.find({}).select(
       '-_id id firstName familyName kidGrade'
     );
-  } catch (err) {
-    console.log('adminRoutes, 21, query error: ', err);
-    res.status(500).json({ error: err.toString() });
-  }
 
-  try {
-    familyDetails = await Family.find({}).select(
+    const familyDetails = await Family.find({}).select(
       '-_id familyId primaryEmail photoConsent addresses familyMedia allKids allParents'
     );
-  } catch (err) {
-    console.log('adminRoutes, 47, query error: ', err);
-    res.status(500).json({ error: err.toString() });
-  }
 
-  try {
-    kidsByFamily = await Family.aggregate([
+    const kidsByFamily = await Family.aggregate([
       {
         $project: {
           _id: 0,
@@ -118,13 +84,8 @@ router.get('/', requireLogin, requireAdmin, async function(req, res) {
       },
       { $unwind: '$kidId' }
     ]);
-  } catch (err) {
-    console.log('adminRoutes, 47, query error: ', err);
-    res.status(500).json({ error: err.toString() });
-  }
 
-  try {
-    parentsByFamily = await Family.aggregate([
+    const parentsByFamily = await Family.aggregate([
       {
         $project: {
           _id: 0,
@@ -134,77 +95,25 @@ router.get('/', requireLogin, requireAdmin, async function(req, res) {
       },
       { $unwind: '$ParentId' }
     ]);
-  } catch (err) {
-    console.log('adminRoutes, 47, query error: ', err);
-    res.status(500).json({ error: err.toString() });
-  }
 
-  try {
-    NoPhotoconsentKids = await Family.distinct('allKids', {
+    const NoPhotoconsentKids = await Family.distinct('allKids', {
       photoConsent: false
     });
-  } catch (err) {
-    res.status(500).json({ error: err.toString() });
-  }
 
-  try {
-    FamiliesNotRegistered = await Family.distinct('familyId', {
+    const FamiliesNotRegistered = await Family.distinct('familyId', {
       registeredEvents: []
     });
-  } catch (err) {
-    res.status(500).json({ error: err.toString() });
-  }
 
-  // TODO get familiesRegistered as families who registered 'i0'
-  try {
-    FamiliesRegistered = await Family.distinct('familyId', {
+    // TODO get familiesRegistered as families who registered 'i0'
+    const FamiliesRegistered = await Family.distinct('familyId', {
       registeredEvents: { $ne: [] }
     });
-  } catch (err) {
-    res.status(500).json({ error: err.toString() });
-  }
 
-  try {
-    // kidsQuantity = await User.find({kidGrade: {$exists: 1}}).countDocuments();
-    kidsQuantity = await User.find({
+    const kidsQuantity = await User.find({
       kidGrade: { $ne: null }
     }).countDocuments();
-  } catch (err) {
-    // console.log('adminRoutes, 29, query error: ', err);
-    console.log('adminRoutes, adsfgh, error: ', err.toString());
-    res.status(500).json({ error: err.toString() });
-  }
 
-  try {
-    parentsQuantity = await User.find({
-      kidGrade: null
-    }).countDocuments();
-  } catch (err) {
-    // console.log('adminRoutes, 29, query error: ', err);
-    console.log('adminRoutes, adsfgh, error: ', err.toString());
-    res.status(500).json({ error: err.toString() });
-  }
-
-  let registrationItems = ['i0'];
-  let classItems = ['i1', 'i2', 'i3', 'i4', 'i5', 'i6', 'i7', 'i8'];
-  let volunteeringItems = [
-    'i9',
-    'i10',
-    'i11',
-    'i12',
-    'i13',
-    'i14',
-    'i15',
-    'i16',
-    'i17',
-    'i18',
-    'i19',
-    'i20',
-    'i21'
-  ];
-  // TODO get classItems and volunteeringItems from Asso document
-  let registrationsByItem = {};
-  try {
+    let registrationsByItem = {};
     await Promise.all(
       []
         .concat(registrationItems, classItems, volunteeringItems)
@@ -217,28 +126,14 @@ router.get('/', requireLogin, requireAdmin, async function(req, res) {
           );
         })
     );
-  } catch (err) {
-    res.status(500).json({ error: err.toString() });
-  }
 
-  try {
-    kidsInClasses = await Registration.distinct('clientId', {
+    const kidsInClasses = await Registration.distinct('clientId', {
       clientId: {
         $nin: FamiliesRegistered
       }
     });
-  } catch (error) {
-    res.status(500).json({ error: err.toString() });
-  }
 
-  let allKidGrades = [
-    ['PS', 'MS', 'GS'],
-    ['CP', 'CE1', 'CE2', 'CM1', 'CM2'],
-    ['6e', '5e', '4e', '3e']
-  ];
-  // TODO get allKidGrades from Asso document
-  let KidsByGrade = {};
-  try {
+    let KidsByGrade = {};
     await Promise.all(
       [...new Set([].concat(...allKidGrades))].map(async kidGrade => {
         KidsByGrade[kidGrade] = await User.distinct('id', {
@@ -246,14 +141,9 @@ router.get('/', requireLogin, requireAdmin, async function(req, res) {
         });
       })
     );
-  } catch (err) {
-    res.status(500).json({ error: err.toString() });
-  }
 
-  try {
     // volunteers = await Registration.find({itemId: { $in: volunteeringItems }}).select('-_id itemId clientId');
-
-    volunteers = await Registration.aggregate([
+    const volunteers = await Registration.aggregate([
       { $project: { _id: 0, itemId: 1, clientId: 1 } },
       { $match: { itemId: { $in: volunteeringItems } } },
       {
@@ -270,31 +160,26 @@ router.get('/', requireLogin, requireAdmin, async function(req, res) {
         }
       }
     ]);
-  } catch (err) {
-    res.status(500).json({ error: err.toString() });
-  }
 
-  res.status(201).json({
-    kidsInClasses,
-    volunteers,
-    KidsByGrade,
-    registrationItems,
-    classItems,
-    volunteeringItems,
-    registrationsByItem,
-    FamiliesRegistered,
-    FamiliesNotRegistered,
-    NoPhotoconsentKids,
-    usersDetails,
-    kidsByFamily,
-    parentsByFamily,
-    familyDetails,
-    itemDetails,
-    kidsQuantity,
-    parentsQuantity,
-    KidsNoPhoto,
-    csvexport
-  });
-});
+    res.status(201).json({
+      kidsInClasses,
+      volunteers,
+      KidsByGrade,
+      registrationItems,
+      classItems,
+      volunteeringItems,
+      registrationsByItem,
+      FamiliesRegistered,
+      FamiliesNotRegistered,
+      NoPhotoconsentKids,
+      usersDetails,
+      kidsByFamily,
+      parentsByFamily,
+      familyDetails,
+      itemDetails,
+      kidsQuantity
+    });
+  })
+);
 
 module.exports = router;
